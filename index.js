58759ba69634ed44ff7d0b79337985556dc6a7dd
Ling Qing Meng <ling.q.meng@gmail.com>
2022-03-28 22:59:05 -0700

    clean up tmrw

--- index.js
+++ index.js
@@ -19,25 +19,42 @@ app.get('/auth', (req, res) => {
   );
 });
 
+app.get('/oauth_redirect',function (req,res) {
+  console.log("req: ",req);
+})
+
 app.get('/oauth_callback', (req, res) => {
   let {url} = req;
-  let code = url.split('?')[1]; // needs error checking
+
+  console.log("url: ",url);
+  let precode = url.split('?')[1]; // needs error checking
+  let code = precode.split('=')[1]; // needs error checking
   let { query } = req;
   const body = {
     client_id: process.env.GITHUB_CLIENT_ID,
+    // redirect_uri='http://localhost:3000/oauth_redirect',
     client_secret: process.env.GITHUB_SECRET,
     code,
   };
-  const opts = { headers: { accept: 'application/json' } };
+  const opts = { 
+    headers: { 
+      'Content-Type': 'application/json', 
+      'accept': 'application/json' 
+    } 
+  };
   axios
     .post('https://github.com/login/oauth/access_token', body, opts)
-    .then((response) => response.data.access_token)
+    .then((_res) => { 
+      return _res.data.access_token
+    })
     .then((token) => {
       console.log('My token:', token);
       res.redirect(`/?token=${token}`);
     })
     .catch((err) => res.status(500).json({ err: err.message }));
 });
+
+
 
 app.listen(3000);
 console.log('App listening on port 3000');
